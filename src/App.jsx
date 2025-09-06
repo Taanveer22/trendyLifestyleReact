9ba3165dbf8b4b8c4d7462478c24b0d71b371fd5
Taanveer22 <taanveer469@gmail.com>
2025-09-06 14:30:29 +0600

    alhamdulliah full poject done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,68 @@
-// import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import AppChild from "./AppChild";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("./fakeData.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleCart = (element) => {
+    // console.log(element);
+    const isExist = cart.find((fe) => fe.id === element.id);
+    console.log(isExist);
+    if (!isExist) {
+      setCart([...cart, element]);
+    } else {
+      alert("already exist");
+    }
+  };
+
+  const handleDelete = (clickId) => {
+    // console.log(clickId);
+    const newCart = cart.filter((item) => item.id !== clickId);
+    setCart(newCart);
+  };
+  // console.log(products);
 
   return (
     <>
-      <AppChild></AppChild>
+      <section id="main-container">
+        <div className="products-container">
+          {products.map((element, index) => (
+            <AppChild
+              element={element}
+              key={index}
+              handleCart={handleCart}
+            ></AppChild>
+          ))}
+        </div>
+        <div className="cart-container">
+          <h1>Checkout Cart</h1>
+          <div className="cart-heading">
+            <h2>serial</h2>
+            <h2>product</h2>
+            <h2>price</h2>
+            <h2>action</h2>
+          </div>
+          {cart.map((item, index) => (
+            <div className="cart-heading">
+              <h5>{index}</h5>
+              <h5>{item.title}</h5>
+              <h5>{item.price}</h5>
+              <button onClick={() => handleDelete(item.id)} className="btn-2">
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
--- src/AppChild.jsx
+++ src/AppChild.jsx
@@ -1,9 +1,21 @@
-import "./AppChild.css";
+import "./App.css";
 
-const AppChild = () => {
-  return <div>
-    <h1>Lorem ipsum dolor sit amet.</h1>
-  </div>;
+const AppChild = ({ element, handleCart }) => {
+  // console.log(element);
+  const { title, price, description, image } = element;
+  return (
+    <div>
+      <div className="product">
+        <img src={image} alt="" />
+        <h3>{title}</h3>
+        <p>{description.slice(0, 75)}</p>
+        <h3>Price : {price} Taka </h3>
+        <button onClick={() => handleCart(element)} className="btn">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AppChild;
