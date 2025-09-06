492dffad0e6de0c34759c00b03b8b126b39f0fc7
Taanveer22 <taanveer469@gmail.com>
2025-09-06 09:16:32 +0600

    alhamdulliah project setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import "./App.css";
+import AppChild from "./AppChild";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <AppChild></AppChild>
+    </>
+  );
+}
+
+export default App;
---
+++ src/AppChild.jsx
@@ -0,0 +1,9 @@
+import "./AppChild.css";
+
+const AppChild = () => {
+  return <div>
+    <h1>Lorem ipsum dolor sit amet.</h1>
+  </div>;
+};
+
+export default AppChild;
